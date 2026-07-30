5d5ffd994c719392ce86b0cd9d304c96df869e1d
lucascoelh02 <fatec2020.22@gmail.com>
2026-07-30 11:44:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
+import Downsell from "./pages/Downsell.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -13,10 +14,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          <Route path="/downsell" element={<Downsell />} />
+
+          {/* Mantenha esta rota sempre por último */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
